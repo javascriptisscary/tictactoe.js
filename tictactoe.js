91a24fe4c32417c7ddf4ca7aac18bab7b81d58f0
Kenneth <crazedpokerpro@yahoo.com>
2017-06-15 16:04:37 -0500

    html canvas animation for xs and os

--- tictactoe.js
+++ tictactoe.js
@@ -72,47 +72,82 @@ function boardClick() {
         
         case "one":
           console.log("1");
-          target.innerHTML ="X";
+          drawMark(target);
           break;
         case "two":
           console.log("2");
+          drawMark(target);
           break;
         case "three":
           console.log("3");
+          drawMark(target);
           break;
         case "four":
           console.log("4");
+          drawMark(target);
           break;
         case "five":
           console.log("5");
+          drawMark(target);
           break;
         case "six":
           console.log("6");
+          drawMark(target);
           break;
         case "seven":
           console.log("7");
+          drawMark(target);
           break;
         case "eight":
           console.log("8");
+          drawMark(target);
           break;
         case "nine":
           console.log("9");
+          drawMark(target);
           break;
       }
     });
   } //end of boardclick()
+  
+  function drawMark(square)
+  {
+    //square.innerHTML = "X";
+    // https://stackoverflow.com/questions/29911143/how-can-i-animate-the-drawing-of-text-on-a-web-page
+    // https://stackoverflow.com/questions/15661339/how-do-i-fix-blurry-text-in-my-html5-canvas
+    var ctx = square.getContext("2d"),
+    dashLen = 100, dashOffset = dashLen, speed = 3,
+    txt = "x", x = 25, i = 0; //make me dynamic
+
+    ctx.font = "100px Chewy, cursive";
+    //ctx.globalAlpha = 0.95;
+    ctx.strokeStyle = ctx.fillStyle = "white";
+
+    (function loop() {
+      ctx.clearRect(x, 0, 60, 150);
+      ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
+      dashOffset -= speed;                                         // reduce dash length
+      ctx.strokeText(txt[i], x, 80);                               // stroke letter
+
+    if (dashOffset > 0) requestAnimationFrame(loop);             // animate
+    else {
+      ctx.fillText(txt[i], x, 80);                               // fill final letter
+      
+  }
+})();
+    
+    
+    
+  }
+  
 
 function init()
 {
-    
-    console.log("running");
   // init squares
   for (var i=1; i < 10; ++i) {
     squareArray.push(new Square(i, false, false));
   }
   
-  
-  console.log(squareArray);
   new Board(squareArray);
   boardClick();
   
